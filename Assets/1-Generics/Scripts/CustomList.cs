645287ab911016063e2ea3b4e592aca6e00696a6
NTam <nichole.cy.tam@gmail.com>
2017-07-24 12:48:57 +1000

    Added Generics

---
+++ Assets/1-Generics/Scripts/CustomList.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generics
+{
+    [System.Serializable]
+    public class CustomList<T> // specifing this class is going to be a generic class
+    {
+        public T[] list;
+        public int capacity { get; }
+        public int amount { get; private set; }
+        //Default constructor
+        public CustomList() { }
+        // gameObjects[0]
+        public T this[int index]
+        {
+            get
+            {
+                return list[index];
+            }
+            set
+            {
+                list[index] = value;
+            }
+        }
+
+        // Adds item to the end of the CustomList<T>
+        public void Add(T item)
+        {
+            // Create a new array of amount + 1
+            T[] cache = new T[amount + 1];
+            //Check if list has been initialised
+            if (list != null)
+            {
+                // Copy all existing items to new array
+                for (int i = 0; i < list.Length; i++)
+                {
+                    cache[i] = list[i];
+                }
+            }
+            // Place new item at end index
+            cache[amount] = item;
+            // Replace old array with new array
+            list = cache;
+            //Increment amount
+            amount++;
+        }
+    }
+}
