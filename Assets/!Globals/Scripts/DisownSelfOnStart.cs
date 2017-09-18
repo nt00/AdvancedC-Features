14812690e7b8ed80af7132773ff5a9e5b070c791
NTam <nichole.cy.tam@gmail.com>
2017-09-18 12:25:03 +1000

    Added Golf with Manny

---
+++ Assets/!Globals/Scripts/DisownSelfOnStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GolfWithManny
+{
+    public class DisownSelfOnStart : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            // bye parents
+            transform.SetParent(null);
+        }
+    }
+}
---
+++ Assets/~GolfWithManny/Scripts/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace GolfWithManny
+{
+    [RequireComponent(typeof(Player))]
+    public class Controller : NetworkBehaviour
+    {
+        public Camera cam;
+        private Player p;
+
+        void Awake()
+        {
+            p = GetComponent<Player>();
+        }
+
+        void Start()
+        {
+            if (!isLocalPlayer)
+            {
+                cam.gameObject.SetActive(false);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(isLocalPlayer)
+            {
+                Vector3 camForward = cam.transform.forward;
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    p.Move(camForward);
+                }
+            }
+        }
+    }
+}
